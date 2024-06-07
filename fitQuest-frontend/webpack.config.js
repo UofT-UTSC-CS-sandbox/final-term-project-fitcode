e8682f9f5ca6babbb7eabac294452b45b8c7c29e
jarvis492 <96554786+jarvis492@users.noreply.github.com>
2024-06-06 21:18:09 -0400

    feat: Added webpack compatability to project

---
+++ fitQuest-frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "/src/index.jsx",
+  output: { path: path.resolve(__dirname, "../fitQuest/core/static/js") },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve("./src"), path.resolve("./node_modules")],
+    extensions: [".js", ".jsx"],
+  },
+};
