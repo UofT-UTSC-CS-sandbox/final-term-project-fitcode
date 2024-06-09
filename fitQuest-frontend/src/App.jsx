bfe2f1b6511e6b10d189092b87d5d709dcd1fc3d
In Kim <92496952+kmstr87@users.noreply.github.com>
2024-06-09 05:40:56 -0400

    feat: Added the ComponentButton component with its styling and the font link needed for it (#6)

--- fitQuest-frontend/src/App.jsx
+++ fitQuest-frontend/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import React from 'react';
-
+import { useState } from "react";
+import React from "react";
+import ComponentButton from "./UI/ComponentButton/ComponentButton";
 
 function App() {
-  return (
-    <>
-      Test Page
-    </>
-  )
+  return <>Test Page</>;
 }
 
-export default App
+export default App;
