1570a3c6ac9236992ed99d2147fe6244a42f0952
Francis Gajitos <FrancisJed@outlook.com>
2024-06-09 17:06:16 -0400

    Chore: Modified FormButton to match Figma styling

--- fitQuest-frontend/src/UI/FormButton/FormButton.jsx
+++ fitQuest-frontend/src/UI/FormButton/FormButton.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./FormButton.css";
 
+/* For this button, there are only two options for text: Log In and Sign Up.
+   The prop formButtonType indicates the role of the button in the forms.
+   The default value for the text for all FormButtons is "Log In".
+   However, you can switch the text to "Sign Up" by inputting "s" as the 
+   prop formButtonType.
+ */
+
 const FormButton = ({ formButtonType }) => {
-    let text = "";
-    if (formButtonType) formButtonType === "login" ? text = "Log In" : text = "Sign Up";
+    let text = "Log In";
+    if (formButtonType === "s")    text = "Sign Up";
     return (
         <button className="form-button">{text}</button>
     );
