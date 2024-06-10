163154a2954a13a62ce7ccce0ffa85ecadccbeb0
Francis Gajitos <FrancisJed@outlook.com>
2024-06-10 17:54:34 -0400

    Added ContentComponents to App.jsx for testing

--- fitQuest-frontend/src/App.jsx
+++ fitQuest-frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
+import ContentComponent from "./UI/ContentComponent/ContentComponent";
+import FormButton from "./UI/FormButton/FormButton";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar/>
+      <ContentComponent/>
+      <ContentComponent buttonType={"questList"} text={"Quest List"}/>
+      <ContentComponent buttonType={"otherHome"} text={"Ongoing Quests"}/>
     </>
   );
 }
