0540afcffc39a5084cdf6fbcee379e476bd40d02
Francis Gajitos <FrancisJed@outlook.com>
2024-06-14 15:10:08 -0400

    Feat: Created HomePage front end

--- fitQuest-frontend/src/index.jsx
+++ fitQuest-frontend/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import HomePage from './Pages/HomePage'
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-root.render(<App />);
+root.render(<HomePage />);
