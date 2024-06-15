fcad38d86c95067f50338069aab88aadc0cd53d5
dan-fu2004 <dan.fu@mail.utoronto>
2024-06-15 16:50:36 -0400

    Feat: Created two pages, Quest List and Quest Exercise

--- fitQuest-frontend/src/App.jsx
+++ fitQuest-frontend/src/App.jsx
@@ -5,11 +5,12 @@ import ContentComponent from "./UI/ContentComponent/ContentComponent";
 import FormButton from "./UI/FormButton/FormButton";
 import UserProfile from "./pages/UserProfile/UserProfile";
 import { Route, Routes } from "react-router-dom";
-
+import WorkoutSelection from "./pages/WorkoutSelection/WorkoutSelection";
+import QuestSelect from "./pages/QuestSelect/QuestSelect";
 function App() {
   return (
     <>
-      <UserProfile />
+      <QuestSelect/>
     </>
   );
 }
---
+++ fitQuest-frontend/src/pages/QuestSelect/QuestSelect.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Navbar from "../../component/Navbar/Navbar";
+import ComponentButton from "../../UI/ComponentButton/ComponentButton";
+import ContentComponent from "../../UI/ContentComponent/ContentComponent";
+
+import "../UserProfile/UserProfile.css"
+import "./QuestSelect.css"
+const QuestSelect = () => {
+
+    const handleClick = (value) => {
+        console.log(value);
+      };
+
+    return(
+        <>
+            <Navbar></Navbar>
+            <div className="titleSection">
+                <ComponentButton />
+                <p className="mainTitle">Quests</p>
+                <button className="fillerButton" />
+            </div>
+
+            <div className="questBody">
+                <ContentComponent buttonType= "questExcercise" 
+                text= "Beginner Exercise" value = "100" onClick={() => handleClick(100)} ></ContentComponent>
+
+                <ContentComponent buttonType= "questExcercise" text= "Intermediate Exercise" value = "200"
+                 onClick={() => handleClick(200)} ></ContentComponent>
+
+                <ContentComponent buttonType= "questExcercise" 
+                text= "Advanced Exercise" value = "300" onClick={() => handleClick(300)} ></ContentComponent>
+            </div>
+        </>
+    );
+};
+
+export default QuestSelect;
