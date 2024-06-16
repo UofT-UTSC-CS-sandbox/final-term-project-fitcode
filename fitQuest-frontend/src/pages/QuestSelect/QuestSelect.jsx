5367fe5f8ec73e943bf9dc8bd64a234d6dd8fe54
In Kim <92496952+kmstr87@users.noreply.github.com>
2024-06-16 18:10:24 -0400

    [FIT-52]: Connected Quest List Page to Backend (#25)
    * feat: added backend functionality for quest list
    
    * Chore: Updated .gitignore

--- fitQuest-frontend/src/pages/QuestSelect/QuestSelect.jsx
+++ fitQuest-frontend/src/pages/QuestSelect/QuestSelect.jsx
@@ -1,36 +1,59 @@
-import React from "react";
-import Navbar from "../../component/Navbar/Navbar";
+import React, { useEffect, useState } from "react";
 import ComponentButton from "../../UI/ComponentButton/ComponentButton";
-import ContentComponent from "../../UI/ContentComponent/ContentComponent";
 
-import "../UserProfile/UserProfile.css"
-import "./QuestSelect.css"
+import "../UserProfile/UserProfile.css";
+import "./QuestSelect.css";
+
+const typePara = "type=pull";
+
 const QuestSelect = () => {
+  const [availableQuests, setAvailableQuests] = useState([]);
 
-    const handleClick = (value) => {
-        console.log(value);
-      };
+  useEffect(() => {
+    const getQuests = async () => {
+      try {
+        const resp = await fetch(`/available_quests?${typePara}`);
+        const allQuests = await resp.json();
+        setAvailableQuests(allQuests);
 
-    return(
-        <>
-            <div className="titleSection">
-                <ComponentButton />
-                <p className="mainTitle">Quests</p>
-                <button className="fillerButton" />
-            </div>
+        console.log(allQuests);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getQuests();
+  }, []);
 
-            <div className="questBody">
-                <ContentComponent buttonType= "questExcercise" 
-                text= "Beginner Exercise" value = "100" onClick={() => handleClick(100)} ></ContentComponent>
+  const handleClick = (value) => {
+    console.log(value);
+  };
 
-                <ContentComponent buttonType= "questExcercise" text= "Intermediate Exercise" value = "200"
-                 onClick={() => handleClick(200)} ></ContentComponent>
+  const showDifficulty = (points) =>
+    points === 100 ? "easy" : points === 200 ? "medium" : "hard";
 
-                <ContentComponent buttonType= "questExcercise" 
-                text= "Advanced Exercise" value = "300" onClick={() => handleClick(300)} ></ContentComponent>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="titleSection">
+        <ComponentButton />
+        <p className="mainTitle">Quests</p>
+        <button className="fillerButton" />
+      </div>
+
+      <div className="questBody">
+        {availableQuests.map((curQuest) => {
+          return (
+            <ComponentButton
+              key={curQuest.quest_id}
+              buttonType="main"
+              points={curQuest.quest_points.toString()}
+              text={curQuest.name}
+              difficulty={showDifficulty(curQuest.quest_points)}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default QuestSelect;
--- fitQuest/core/views.py
+++ fitQuest/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
-
+from core.models import Quests
 
 # Create your views here.
 
@@ -25,3 +25,8 @@ def profileData(request):
         "username": user.username,
         "points": profile.points
     })
+
+def availableQuests(request):
+    quest_type = request.GET.get('type')
+    availableQuests = Quests.objects.filter(available=True, target_area=quest_type).values('quest_id', 'target_area', 'name', 'description', 'quest_points')
+    return JsonResponse(list(availableQuests), safe=False)
