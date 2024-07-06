5180dab7454e1a14e170f3a19928e706a5d13429
Francis Gajitos <FrancisJed@outlook.com>
2024-07-06 01:17:06 -0400

    Feat: Implemented accordion when a user clicks quest in QuestList

--- fitQuest-frontend/src/UI/ComponentButton/ComponentButton.js
+++ fitQuest-frontend/src/UI/ComponentButton/ComponentButton.js
@@ -30,16 +30,17 @@ const ComponentButton = ({ buttonType, text, points, difficulty }) => {
 
   // Define toggleAccordion to conditionally toggle based on buttonType
   const toggleAccordion = () => {
-    console.log("hello");
     if (buttonType === "main") {
       setIsAccordionVisible(!isAccordionVisible);
     }
   };
 
+  let accordionClassName = "accordion " + difficulty;
+
   // Simple Accordion Component for demonstration
   const Accordion = () => (
-    <div className="accordion">
-      <p>Quest Description Placeholder</p>
+    <div className={accordionClassName} onClick={toggleAccordion}>
+      <div class="quest-description-text">The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.</div>
       <ComponentButton buttonType="navbar" text="Accept Quest" />
     </div>
   );
