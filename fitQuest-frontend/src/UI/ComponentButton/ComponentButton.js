1b3ee9c251a99b4edb7d223cb3cf28af722b393d
Francis Gajitos <FrancisJed@outlook.com>
2024-07-06 19:45:17 -0400

    Chore: Added border to Accept Quest button

--- fitQuest-frontend/src/UI/ComponentButton/ComponentButton.js
+++ fitQuest-frontend/src/UI/ComponentButton/ComponentButton.js
@@ -25,6 +25,7 @@ const ComponentButton = ({ buttonType, text, points, difficulty }) => {
           : " hard";
       pointsText = points + " points";
     }
+    if (buttonType === "accept") backgroundClass += " acceptBackground";
   }
   if (text) defaultFontText = text;
 
@@ -40,8 +41,8 @@ const ComponentButton = ({ buttonType, text, points, difficulty }) => {
   // Simple Accordion Component for demonstration
   const Accordion = () => (
     <div className={accordionClassName} onClick={toggleAccordion}>
-      <div class="quest-description-text">The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.</div>
-      <ComponentButton buttonType="navbar" text="Accept Quest" />
+      <div class="quest-description-text">This is where the quest description goes.</div>
+      <ComponentButton  buttonType="accept" text="Accept Quest" />
     </div>
   );
 
